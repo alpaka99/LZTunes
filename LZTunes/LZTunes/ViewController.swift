896c5994afd6745c8656ef030880614ea948a760
Lets <ruvan123@naver.com>
2024-08-09 00:49:15 +0900

    Feat: LZTunes 프로젝트 초기화

---
+++ LZTunes/LZTunes/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LZTunes
+//
+//  Created by user on 8/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBlue
+        navigationItem.title = "VC"
+    }
+
+
+}
+

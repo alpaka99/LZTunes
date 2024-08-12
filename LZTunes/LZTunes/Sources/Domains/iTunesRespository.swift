6afa84f458f57666f1bd5621e1888615cb2be799
Lets <ruvan123@naver.com>
2024-08-13 08:10:00 +0900

    Refactor: 데이터 받아오는 과정을 Single로 변경

--- LZTunes/LZTunes/Sources/Domains/iTunesRespository.swift
+++ LZTunes/LZTunes/Sources/Domains/iTunesRespository.swift
@@ -27,22 +27,34 @@ final class iTunesRepository {
                 iTunesRouter.search(searchText: $0)
             }
             .debug()
-            .bind(to: NetworkManager.shared.router)
+//            .bind(to: NetworkManager.shared.router)
+            .flatMap {
+                NetworkManager.shared.requestCallWithSingle(router: $0)
+                    .catch { error in // .failure를 통한 error는 여기서 걸러냄
+                        return Single<Data>.never()
+                    }
+            }
+            .bind(with: self, onNext: { owner, data in
+                let decodedData = try? JSONDecoder().decode(iTunesResponse.self, from: data)
+                if let decodedData = decodedData {
+                    owner.searchResult.onNext(decodedData.results)
+                }
+            })
             .disposed(by: disposeBag)
 
         
-        NetworkManager.shared.completeStatus
-            .bind(with: self) { owner, completeStatus in
-                switch completeStatus {
-                case .complete(let data):
-                    let decodedData = try? JSONDecoder().decode(iTunesResponse.self, from: data)
-                    if let decodedData = decodedData {
-                        owner.searchResult.onNext(decodedData.results)
-                    }
-                case .error(let error):
-                    print("Error", error)
-                }
-            }
-            .disposed(by: disposeBag)
+//        NetworkManager.shared.completeStatus
+//            .bind(with: self) { owner, completeStatus in
+//                switch completeStatus {
+//                case .complete(let data):
+//                    let decodedData = try? JSONDecoder().decode(iTunesResponse.self, from: data)
+//                    if let decodedData = decodedData {
+//                        owner.searchResult.onNext(decodedData.results)
+//                    }
+//                case .error(let error):
+//                    print("Error", error)
+//                }
+//            }
+//            .disposed(by: disposeBag)
     }
 }
